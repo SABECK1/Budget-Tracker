58f99adf8f9a43db7154ae3db1f72f341ef7dbcf
SABECK1 <SamuelBeck16@gmail.com>
2025-08-31 01:25:19 +0200

    Better Footer

--- tracker_vue/src/components/footer.vue
+++ tracker_vue/src/components/footer.vue
@@ -1,9 +1,8 @@
 <template>
-<footer class="bg-body-tertiary text-center fixed-bottom">
-  <div class="container p-4"></div>
+<footer class="fixed-bottom">
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
     © 2025 Copyright:
-    <a class="text-body" href="https://SamuelBeck.software">Samuel Beck</a>
+    <a href="https://SamuelBeck.software">Samuel Beck</a>
   </div>
 </footer>
 </template>
