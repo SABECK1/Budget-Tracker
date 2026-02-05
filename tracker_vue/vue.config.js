2948079b50b9a34d0c0ace8b4410ce26b3aa74f8
SABECK1 <samuelbeck16@gmail.com>
2026-02-06 00:07:57 +0100

    Debugging Config

--- tracker_vue/vue.config.js
+++ tracker_vue/vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    runtimeCompiler: true,
-    transpileDependencies: []
+    transpileDependencies: true,
+    configureWebpack: {
+        // This produces high-quality source maps that the VS Code debugger can read
+        devtool: 'source-map',
+        output: {
+            devtoolModuleFilenameTemplate: info => {
+                return `webpack:///${info.resourcePath}`
+            }
+        }
+    }
 })
